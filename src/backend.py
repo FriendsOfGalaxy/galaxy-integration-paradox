4adad518bfa91cc7f3d273458924d1242f54446e
Tracy <7706977+barthisius@users.noreply.github.com>
2019-12-19 02:29:26 -0600

    Support for (more) games purchased through Steam (#5)

--- src/backend.py
+++ src/backend.py
@@ -4,6 +4,14 @@ import logging as log
 class ParadoxClient:
     def __init__(self, http_client):
         self.http_client = http_client
+        self._skus = {}
+
+    async def get_skus(self):
+        if not self._skus:
+            skus = await self.http_client.do_request('GET', 'https://accounts.paradoxplaza.com/api/skus',
+                                                     headers={'content-type': 'application/json'})
+            self._skus = await skus.json()
+        return self._skus
 
     async def get_account_id(self):
         data = {'Authorization': f'{{"session":{{"token":"{self.http_client.token}"}}}}',
@@ -19,6 +27,7 @@ class ParadoxClient:
                                                      headers=data)
 
         response = await response.json()
+        skus = await self.get_skus()
 
         owned_products = []
         if 'products' in response:
@@ -26,9 +35,11 @@ class ParadoxClient:
                 for platform in platforms:
                     for game in platforms[platform]:
                         log.info(game)
-                        if game['sku'] and game['title'] and game['product_type'] and "paradox builds" in game['platforms']:
-                            owned_products.append({'sku': game['sku'],
-                                                   'title': game['title'],
-                                                   'type': game['product_type']})
+                        if game['sku'] and game['title'] and game['product_type']:
+                            if (game['sku'] in skus and 'paradoxLauncher' in skus[game['sku']]['platform']) \
+                                    or "paradox builds" in game['platforms']:
+                                owned_products.append({'sku': game['sku'],
+                                                       'title': game['title'],
+                                                       'type': game['product_type']})
         log.info(owned_products)
         return owned_products
